b2526405d6757cdb79c9a04b8957332990ae330f
Joe Bennett <joe@assimtech.com>
2021-06-30 16:58:35 +1000

    Added support to obtain lastApiCallId from LoggingHttpClient

--- src/LoggingHttpClient.php
+++ src/LoggingHttpClient.php
@@ -12,12 +12,22 @@ class LoggingHttpClient implements LoggingHttpClientInterface
     private Http\Client\ClientInterface $httpClient;
     private ApiCallLoggerInterface $apiCallLogger;
 
+    private ?string $lastApiCallId = null;
+
     public function __construct(
         Http\Client\ClientInterface $httpClient,
         ApiCallLoggerInterface $apiCallLogger
     ) {
         $this->httpClient = $httpClient;
         $this->apiCallLogger = $apiCallLogger;
+    }
+
+    /**
+     * @api
+     */
+    public function getLastApiCallId(): ?string
+    {
+        return $this->lastApiCallId;
     }
 
     /**
@@ -30,16 +40,20 @@ class LoggingHttpClient implements LoggingHttpClientInterface
         /* callable[]|callable */ $requestProcessors = [],
         /* callable[]|callable */ $responseProcessors = []
     ): Http\Message\ResponseInterface {
-        $loggedApiCall = (null !== $appMethod)
-            ? $this->apiCallLogger->logRequest(
-                GuzzlePsr7\Message::toString($request),
-                (string) $request->getUri(),
-                $appMethod,
-                $reference,
-                $requestProcessors
-            )
-            : null
-        ;
+        $this->lastApiCallId = null;
+        $loggedApiCall = null;
+
+        if (null !== $appMethod) {
+            $loggedApiCall = $this->apiCallLogger->logRequest(
+                    GuzzlePsr7\Message::toString($request),
+                    (string) $request->getUri(),
+                    $appMethod,
+                    $reference,
+                    $requestProcessors
+                )
+            ;
+            $this->lastApiCallId = (string) $loggedApiCall->getId();
+        }
 
         $response = $this->httpClient->sendRequest($request);
         if (null !== $loggedApiCall) {
