8ecc2a0b07c947212476a81ac9ef46f9ecf5628f
Joe Bennett <joe@assimtech.com>
2021-08-12 16:38:15 +1000

    Added MultilineStringSerializer

---
+++ src/Serializer/MultilineStringSerializer.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Assimtech\Dislog\Serializer;
+
+use Assimtech\Dislog;
+
+class MultilineStringSerializer implements SerializerInterface
+{
+    protected $eol;
+
+    public function __construct(
+        string $eol = "\n"
+    ) {
+        $this->eol = $eol;
+    }
+
+    public function __invoke(
+        Dislog\Model\ApiCallInterface $apiCall
+    ): string {
+        return \implode(' ', [
+            "[{$apiCall->getRequestDateTime()->format('c')}]",
+            "({$apiCall->getId()})",
+            $apiCall->getMethod(),
+            "({$apiCall->getEndpoint()})",
+            '|',
+            "({$apiCall->getReference()})",
+            '-',
+            "{$apiCall->getDuration()}"
+        ]) . $this->eol . $this->eol
+            . $apiCall->getRequest() . $this->eol . $this->eol
+            . $apiCall->getResponse() . $this->eol
+            . '---' . $this->eol . $this->eol
+        ;
+    }
+}
